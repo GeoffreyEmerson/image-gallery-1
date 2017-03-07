f3919b735bcd3a921316c4d7f3e9a066a94fa53e
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2017-03-06 16:42:37 -0800

    start conversion to assignment basics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Toolbar from './Toolbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className='App'>
+        <div className='App-header'>
+          <h2>Image Gallery App</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Toolbar />
       </div>
     );
   }
---
+++ src/Toolbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Toolbar.css';
+
+class Toolbar extends Component {
+  render() {
+    return (
+        <p className='Toolbar'>
+          Add toolbar options here.
+        </p>
+    );
+  }
+}
+
+export default Toolbar;
